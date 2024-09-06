e80221b5bff392c2644fa4d4ce40c583fc5ace4f
Sushmavaidya <sushmarvaidya5@gmail.com>
2024-09-06 11:51:11 +0530

    first commit

---
+++ Git_Learning_06_Aug/src/test/java/test_runner/Runner.java
@@ -0,0 +1,12 @@
+package test_runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= {".//Features/login.feature"},glue="step_definitions")
+public class Runner {
+
+}
